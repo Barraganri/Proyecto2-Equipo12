f7fc993dc53eaa96a42a0a104dd3a3d0d5c9c571
Tania Gomez <90874522+TaniaGomez01@users.noreply.github.com>
2021-12-05 03:51:11 -0600

    Update AVLNode.java

--- Proyecto2_Equipo12/src/main/java/com/mycompany/proyecto2_equipo12/AVLNode.java
+++ Proyecto2_Equipo12/src/main/java/com/mycompany/proyecto2_equipo12/AVLNode.java
@@ -8,25 +8,17 @@ package com.mycompany.proyecto2_equipo12;
 public class AVLNode {
     
     int valor, altura;
-    AVLNode izq = null;
-    AVLNode der = null;
+    AVLNode izq;
+    AVLNode der;
     
+    /**
+     * Método constructor, reasigna la información, establece el valor inicial
+     * de nuestra altura
+     * @param data valor del nodo
+     */
     public AVLNode(int data){
-        this(data,null,null);
-    }
-    public AVLNode(int data, AVLNode lt, AVLNode rt){
         valor=data;
-        izq = lt;
-        der = rt;
-        altura=0;
-    }   
-    
-    public void setIzq(AVLNode izq) {
-        this.izq = izq;
-    }
-    
-    public void setDer(AVLNode der) {
-        this.der = der;
-    }
+        altura = 0;
+    } 
 
 }
