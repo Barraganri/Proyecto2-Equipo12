9aa8beb6f198a662de97d2a25db1799a0d5b40b8
Daniela Ramírez <72423222+Dani4890UNAM@users.noreply.github.com>
2021-12-05 14:36:15 -0600

    Update NodoHeap.java

--- Proyecto2_Equipo12/src/main/java/com/mycompany/proyecto2_equipo12/NodoHeap.java
+++ Proyecto2_Equipo12/src/main/java/com/mycompany/proyecto2_equipo12/NodoHeap.java
@@ -8,42 +8,62 @@ package com.mycompany.proyecto2_equipo12;
 
 /**
  *
- * @author Edgar
+ * @author dani_
  */
 public class NodoHeap {
-    
+    /**
+     * Instanciado de Variables
+     */
     int valor;
     NodoHeap izq = null;
     NodoHeap der = null;
     
+    /**
+     * Constructor para la declaracion del Nodo
+     */
     public NodoHeap(){
         izq=der=null;
     }
   
+    /**
+     * Constructor para la declaracion del Nodo
+     * @param data Número entero valor del Nodo
+     */
     public NodoHeap(int data){
         this(data,null,null);
 
     }
-    
-    public NodoHeap(NodoHeap n){
-        this(n.valor,n.izq,n.der);
-    }
-     
+   /**
+    * Constructor para la declaracion del Nodo
+    * @param data Número entero valor del Nodo
+    * @param lt Hijo izquierdo del Nodo
+    * @param rt Hijo derecho del Nodo
+    */
     public NodoHeap(int data, NodoHeap lt, NodoHeap rt){
         valor=data;
         izq = lt;
         der = rt;
     }   
-
+    
+    /**
+     * Método de acceso para asignar el valor del Nodo
+     * @param valor Número entero Valor del Nodo
+     */
     public void setValor(int valor) {
         this.valor = valor;
     }
 
-
+    /**
+     * Método de acceso para asignar el hijo izquierdo del Nodo
+     * @param izq Hijo Izquierdo del Nodo
+     */
     public void setIzq(NodoHeap izq) {
         this.izq = izq;
     }
-    
+    /**
+     * Método de acceso para asignar el hijo derecho del Nodo
+     * @param der Hijo Derecho del Nodo
+     */
     public void setDer(NodoHeap der) {
         this.der = der;
     }
