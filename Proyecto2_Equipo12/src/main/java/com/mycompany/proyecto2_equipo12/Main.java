d86a93e638ca182d35114d808edaecfb9631ce05
tania <tania@LAPTOP-D1DFTLO0>
2021-12-04 22:11:17 -0600

    Merge origin/main into main
    Conflicts:
    	Proyecto2_Equipo12/src/main/java/com/mycompany/proyecto2_equipo12/a.java

--- Proyecto2_Equipo12/src/main/java/com/mycompany/proyecto2_equipo12/Main.java
+++ Proyecto2_Equipo12/src/main/java/com/mycompany/proyecto2_equipo12/Main.java
@@ -42,7 +42,7 @@ public class Main {
             System.out.println("\t\t|\t4. Salir  \t\t\t\t|");
             System.out.println("\t\t-------------------------------------------------");
             
-            System.out.println("\nElige una opción1:     ");
+            System.out.println("\nElige una opción1 :D:     ");
             opcion = sn.nextInt();
             
             switch(opcion){
